23641f3fe7f0b561c746e96b709efc5dd6ea50db
Nenny Dike <nennydike@gmail.com>
2019-06-23 20:49:53 +0100

    165726917-story(user):signup integration - users can signup with the UI [Finishes #165726917]

--- public/js/index.js
+++ public/js/index.js
@@ -27,28 +27,16 @@ const logMeIn = async (e) => {
       content.innerText = json.message;
       return;
     }
-    const { data, message } = await response.json();
-    const { user, token } = data;
-    const localData = {
-      token,
-      id : user.id,
-      firstname : user.firstname,
-      surname : user.surname,
-      phonenumber: user.phonenumber,
-      permission: user.permission,
-      isadmin: user.isadmin,
-      email: user.email,
-      imgurl: user.imgurl,
-    }    
-    localStorage.setItem("user", JSON.stringify(localData))
-        console.log(user);
-    const { permission} = user;
-    if (permission === 'ADMIN') {
-      content.innerText = message;
+    
+    const {data} = await response.json();
+    const { token, user} = data;
+    localStorage.setItem('token', token);
+    localStorage.setItem("user", JSON.stringify(user));
+    
+    if (user.permission === 'ADMIN') {
       window.location.href = "user/dashboard-admin.html";
-
-    } if (permission === 'STAFF') {
-      content.innerText = message;
+      return false;
+    } if (user.permission === 'STAFF') {
       window.location.href = "user/dashboard-staff.html";
       return false;
     } else {
@@ -60,6 +48,73 @@ const logMeIn = async (e) => {
   } catch (error) {
     hideSpinner();
     content.innerText = `Failed to retrieve user informations: ${error}`;
+  }
+
+  return false;
+};
+
+const signMeUp = async (e) => {
+  e.preventDefault();
+  const firstname = document.getElementById("firstname").value;
+  const surname = document.getElementById("surname").value;
+  const email = document.getElementById("userEmail").value;
+  const phonenumber = document.getElementById("phoneNumber").value;
+  const password = document.getElementById("password").value;
+  const confirm_password = document.getElementById("confirm-password").value;
+  const content = document.getElementById("login-respond");
+  console.log(firstname, surname, email, phoneNumber, password, confirm_password);
+
+  const url = "https://banka-nenny.herokuapp.com/api/v1/auth/signup"
+  const payload = JSON.stringify({ firstname, surname, email, phonenumber, password })
+  console.log(payload);
+
+  // showSpinner();
+
+  try {
+    if (password !== confirm_password) {
+      content.style.display = 'block';
+      content.innerText = 'password does not match confirm password';
+      return;
+    }
+    const response = await fetch(url,
+      {
+        method: 'POST',
+        body: payload,
+        mode: "cors",
+        cache: "no-cache",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    if (!response.ok) {
+      // hideSpinner();
+
+      const json = await response.json()
+
+      content.style.display = 'block';
+      content.innerText = json.message;
+      return;
+    }
+
+   const { data } = await response.json();
+    console.log(data);
+  
+    localStorage.setItem("user", JSON.stringify(data));
+
+    if (data.permission === 'ADMIN') {
+      window.location.href = "dashboard-admin.html";
+      return false;
+    } if (data.permission === 'STAFF') {
+      window.location.href = "dashboard-staff.html";
+      return false;
+    } else {
+      window.location.href = "profile.html";
+      return false;
+    }
+
+  } catch (error) {
+    // hideSpinner();
+    console.error(`Failed to retrieve user informations: (${error})`);
   }
 
   return false;
